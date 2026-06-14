169a7e75c9d83c315cc3fa206d2a0c132d2a098f
front-ente <front-ente@users.noreply.github.com>
2026-06-14 19:34:18 +0200

    add AuthResponseSchema, change quotes

--- shared/schemas/user.schemas.ts
+++ shared/schemas/user.schemas.ts
@@ -1,26 +1,26 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const createUserSchema = z.object({
   email: z.email(),
   password: z.string().min(8),
-})
+});
 
 export const loginSchema = z.object({
   email: z.email(),
   password: z.string().min(1),
-})
+});
 
 export const userSchema = z.object({
   _id: z.string(),
   email: z.email(),
-})
+});
 
 export const authResponseSchema = z.object({
   user: userSchema,
   accessToken: z.string(),
-})
+});
 
-export type CreateUserInput = z.infer<typeof createUserSchema>
-export type LoginInput = z.infer<typeof loginSchema>
-export type User = z.infer<typeof userSchema>
-export type AuthResponse = z.infer<typeof authResponseSchema>
+export type CreateUserInput = z.infer<typeof createUserSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
+export type User = z.infer<typeof userSchema>;
+export type AuthResponse = z.infer<typeof authResponseSchema>;
