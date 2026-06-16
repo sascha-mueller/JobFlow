44a96d72472b586de228d12275fc8de217a90c6f
front-ente <front-ente@users.noreply.github.com>
2026-06-16 16:02:55 +0200

    add login and registration pages react-hook-form + zod validation, password strength meter, auth layout styles

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,11 +1,5 @@
 import { Outlet } from "react-router";
 
-const AuthLayout = () => {
-  return (
-    <div className="page-container">
-      <Outlet />
-    </div>
-  );
-};
-
-export default AuthLayout;
+export default function AuthLayout() {
+  return <Outlet />;
+}
